1a6e0187e7b7560c4c72dadcd2ffd52f53544eed
PhamThiNgocHoa <127472698+PhamThiNgocHoa@users.noreply.github.com>
2024-06-09 16:58:42 +0700

    up code

---
+++ app/src/main/java/com/example/myapplication/CartActivity.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CartActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_cart);
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/EditProfileActivity.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditProfileActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_edit_profile);
+
+    }
+}
